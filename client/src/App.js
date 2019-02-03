2c3ce5733f028ae66da10284a18b5ac4574d00fb
Miquel Santandreu Duran <16miquel11@gmail.com>
2019-02-03 15:36:18 +0100

    Gets progress data from props

--- client/src/App.js
+++ client/src/App.js
@@ -1,98 +1,114 @@
-import React, { Component } from 'react';
-import './App.css';
-import {LoginButton} from './LoginButton'
+import React, { Component } from "react";
+import "./App.css";
+import { LoginButton } from "./LoginButton";
 
-import SpotifyWebApi from 'spotify-web-api-js';
-import { SongDisplay } from './displays/SongDisplay';
+import SpotifyWebApi from "spotify-web-api-js";
+import { SongDisplay } from "./displays/SongDisplay";
 const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
-  constructor(){
-    super();
-    const params = this.getHashParams();
-    const token = params.access_token;
-    if (token) {
-      spotifyApi.setAccessToken(token);
-    }
+	constructor() {
+		super();
+		const params = this.getHashParams();
+		const token = params.access_token;
+		if (token) {
+			spotifyApi.setAccessToken(token);
+		}
 
-    this.state = {
-      loggedIn: token ? true : false,
-      nowPlaying: { 
-        name: "Name not checked", 
-        album: {
-          art: "https://music.uberchord.com/assets/images/png/placeholder-song.png",
-          name: "",
-          year: ""
-        },
-        artist: {
-          name: ""
-        },
-        stats: {
-          acoustiness: 0,
-          energy: 0,
-          instrumentalness: 0,
-          danceability: 0
-        }
-      }
-    }
-  }
+		this.props = {
+			loggedIn: token ? true : false
+		};
 
-  getHashParams() {
-    var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    e = r.exec(q)
-    while (e) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-       e = r.exec(q);
-    }
-    return hashParams;
-  }
-  
-  getTrackData(trackId) {
-    spotifyApi.getAudioFeaturesForTrack(trackId)
-      .then((response) => {
-          console.log(response);
-      })
-  }
+		this.state = {
+			nowPlaying: {
+				name: "Name not checked",
+				album: {
+					art:
+						"https://music.uberchord.com/assets/images/png/placeholder-song.png",
+					name: "",
+					year: ""
+				},
+				artist: {
+					name: ""
+				},
+				stats: {
+					acoustiness: 0,
+					energy: 0,
+					instrumentalness: 0,
+					danceability: 0
+				}
+			},
+			song: {
+				acoustiness: 0,
+				energy: 0,
+				instrumentalness: 0,
+				danceability: 0
+			}
+		};
+	}
 
-  getNowPlaying(){
-    spotifyApi.getMyCurrentPlaybackState()
-      .then((response) => {
-        this.setState({
-          nowPlaying: { 
-              id: response.item.id,
-              name: response.item.name, 
-              album: {
-                art: response.item.album.images[0].url,
-                name: response.item.album.name,
-                year: response.item.album.release_date
-              },
-              artist: {
-                name: response.item.artists[0].name
-              }
-            }
-        });
+	getHashParams() {
+		var hashParams = {};
+		var e,
+			r = /([^&;=]+)=?([^&;]*)/g,
+			q = window.location.hash.substring(1);
+		e = r.exec(q);
+		while (e) {
+			hashParams[e[1]] = decodeURIComponent(e[2]);
+			e = r.exec(q);
+		}
+		return hashParams;
+	}
 
-        this.getTrackData(response.item.id);
-      })
-  }
+	getTrackData(trackId) {
+		spotifyApi.getAudioFeaturesForTrack(trackId).then(response => {
+			let updatedSong = {
+				acoustiness: Math.round(response.acousticness * 100),
+				energy: Math.round(response.energy * 100),
+				instrumentalness: Math.round(response.instrumentalness * 100),
+				danceability: Math.round(response.danceability *100)
+			};
 
-  render() {
-    const isLoggedIn = this.state.isLoggedIn;
+			this.setState({ song: updatedSong });
+		});
+	}
 
-    return (
-      <div className="App">
-      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet"></link>
-        {isLoggedIn ? <LoginButton/> : <SongDisplay song={this.state.nowPlaying} />}
-        {this.state.loggedIn &&
-          <button onClick={() => this.getNowPlaying()}>
-            Check Now Playing
-          </button>
-        }
-      </div>
-    );
-  }
+	getNowPlaying() {
+		spotifyApi.getMyCurrentPlaybackState().then(response => {
+			this.setState({
+				nowPlaying: {
+					id: response.item.id,
+					name: response.item.name,
+					album: {
+						art: response.item.album.images[0].url,
+						name: response.item.album.name,
+						year: response.item.album.release_date
+					},
+					artist: {
+						name: response.item.artists[0].name
+					}
+				}
+			});
+
+			this.getTrackData(response.item.id);
+		});
+	}
+
+	render() {
+		const isLoggedIn = this.props.isLoggedIn;
+
+		return (
+			<div className="App">
+				<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet"></link>
+				{isLoggedIn ? <LoginButton /> : <SongDisplay data={this.state} />}
+				{
+					<button onClick={() => this.getNowPlaying()}>
+						Check Now Playing
+					</button>
+				}
+			</div>
+		);
+	}
 }
 
 export default App;
--- client/src/displays/SongDisplay.js
+++ client/src/displays/SongDisplay.js
@@ -1,26 +1,39 @@
-import React, { Component } from 'react';
-import './Song.css';
-import { CustomProgress } from './progress/CustomProgress';
+import React, { Component } from "react";
+import "./Song.css";
+import { CustomProgress } from "./progress/CustomProgress";
 
 export class SongDisplay extends Component {
-    render() {        
-        return (
-            <div className="songHolder">
-                <div className="avatarHolder">
-                    <img className="albumAvatar" src={this.props.song.album.art} alt={this.props.song.name}></img>
-                </div>
-                <div className="songNameHolder">
-                    <p className="title">{this.props.song.name}</p>
-                    <p className="artist">{this.props.song.artist.name}</p>
-                </div>
+	render() {
+		return (
+			<div className="songHolder">
+				<div onClick={this.calculateSongProgress} className="avatarHolder">
+					<img    
+						className="albumAvatar"
+						src={this.props.data.nowPlaying.album.art}
+						alt={this.props.data.nowPlaying.name}
+					/>
+				</div>
+				<div className="songNameHolder">
+					<p className="title">{this.props.data.nowPlaying.name}</p>
+					<p className="artist">{this.props.data.nowPlaying.artist.name}</p>
+				</div>
 
-                <div className="songInfoHolder">
-                    <CustomProgress name="Acousticness" progress={this.props.song.acoustiness}/>
-                    <CustomProgress name="Energy" progress={this.props.song.energy}/>
-                    <CustomProgress name="Instrumentalness" progress={this.props.song.instrumentalness}/>
-                    <CustomProgress name="Danceability" progress={this.props.song.danceability}/>
-                </div>
-            </div>
-        );
-      }
+				<div className="songInfoHolder">
+					<CustomProgress
+						name="Acousticness"
+						progress={this.props.data.song.acoustiness}
+					/>
+					<CustomProgress name="Energy" progress={this.props.data.song.energy} />
+					<CustomProgress
+						name="Instrumentalness"
+						progress={this.props.data.song.instrumentalness}
+					/>
+					<CustomProgress
+						name="Danceability"
+						progress={this.props.data.song.danceability}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
